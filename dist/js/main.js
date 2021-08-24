34b1275f03d4ab555eb9e96a499e8665e44dafa9
wijaya014 <wijaya.agus014@gmail.com>
2021-08-24 09:09:25 +0700

    add x burger

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,15 @@
 console.log("Hello");
+const menuBtn = document.querySelector(".menu-btn");
+const hamburger = document.querySelector(".menu-btn__burger");
+
+let display = false;
+
+menuBtn.addEventListener("click", () => {
+    if(!display){
+        hamburger.classList.add('open');
+        display = true;
+    } else {
+        hamburger.classList.remove('open');
+        display= false;
+    }
+});
