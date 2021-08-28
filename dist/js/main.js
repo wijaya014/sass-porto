07754b5977f959d52251d7eb905127094ace8c60
wijaya014 <wijaya.agus014@gmail.com>
2021-08-28 22:41:40 +0700

    add about page & about scss

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,21 +3,24 @@ const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".menu-btn__burger");
 const nav = document.querySelector(".nav");
 const menuNav = document.querySelector(".menu-nav");
+const navitems = document.querySelectorAll(".menu-nav__item");
 
 let display = false;
 
 menuBtn.addEventListener("click", toggleMenu);
 
-function toggleMenu(){
-    if (!display) {
-        hamburger.classList.add("open");
-        nav.classList.add('open');
-        menuNav.classList.add('open');
-        display = true;
-      } else {
-        hamburger.classList.remove("open");
-        nav.classList.remove('open');
-        menuNav.classList.remove('open');
-        display = false;
-      }
-    }
+function toggleMenu() {
+  if (!display) {
+    hamburger.classList.add("open");
+    nav.classList.add("open");
+    menuNav.classList.add("open");
+    navitems.forEach((item) => item.classList.add("open"));
+    display = true;
+  } else {
+    hamburger.classList.remove("open");
+    nav.classList.remove("open");
+    menuNav.classList.remove("open");
+    navitems.forEach((item) => item.classList.remove("open"));
+    display = false;
+  }
+}
