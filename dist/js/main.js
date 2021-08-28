85b808edde25dd7ed32fca35c004392c8a15505c
wijaya014 <wijaya.agus014@gmail.com>
2021-08-28 14:38:59 +0700

    add and remove classList menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,23 @@
 console.log("Hello");
 const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
 
 let display = false;
 
-menuBtn.addEventListener("click", () => {
-    if(!display){
-        hamburger.classList.add('open');
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu(){
+    if (!display) {
+        hamburger.classList.add("open");
+        nav.classList.add('open');
+        menuNav.classList.add('open');
         display = true;
-    } else {
-        hamburger.classList.remove('open');
-        display= false;
+      } else {
+        hamburger.classList.remove("open");
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        display = false;
+      }
     }
-});
